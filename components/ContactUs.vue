7d7eb39ab148718a41efb1e0d622643b08e6ad3c
theMosi <mostafawshe@gmail.com>
2023-04-07 02:14:20 +0330

    ContactUs

---
+++ components/ContactUs.vue
@@ -0,0 +1,50 @@
+<template>
+        <section class="book_section layout_padding">
+        <div class="container">
+            <div class="heading_container">
+                <h2>
+                    تماس با ما
+                </h2>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form_container">
+                        <form @submit="sendFunc">
+                            <div>
+                                <input type="text" class="form-control" placeholder="نام و نام خانوادگی" />
+                            </div>
+                            <div>
+                                <input type="email" class="form-control" placeholder="ایمیل" />
+                            </div>
+                            <div>
+                                <input type="text" class="form-control" placeholder="موضوع پیام" />
+                            </div>
+                            <div>
+                                <textarea rows="10" style="height: 100px" class="form-control"
+                                    placeholder="متن پیام"></textarea>
+                            </div>
+                            <div class="btn_box">
+                                <button>
+                                    ارسال پیام
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="map_container ">
+                        <div id="map" style="height: 345px;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+let sendFunc = () => {
+    
+}
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,4 +3,5 @@
     <Features />
     <ProductTabs />
     <AboutUs />
+    <ContactUs />
 </template>
