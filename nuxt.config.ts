420d504af3736b156b5701304c278462264eddb3
theMosi <mostafawshe@gmail.com>
2023-04-06 08:38:13 +0330

    product-tabs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,12 @@ export default defineNuxtConfig({
             }
         }
     },
+
+    runtimeConfig:{
+        public:{
+            apiBase: "http://localhost:8000/api"
+        }
+    },
+
     css:["~/assets/css/main.css"],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
     <Slider />
     <Features />
+    <ProductsTab />
 </template>
