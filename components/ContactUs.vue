a7b5c05c4d7dc503efab7bd6a7a704bff1c3e0e3
theMosi <mostafawshe@gmail.com>
2023-04-08 04:41:25 +0330

    ContactUs-map

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -54,6 +54,7 @@ import {useToast} from "vue-toastification";
 const {public:{apiBase}} = useRuntimeConfig();
 
 const toast = useToast();
+const { $leaflet } = useNuxtApp();
 
 const errors = ref([]);
 const loading = ref(false);
@@ -86,5 +87,26 @@ async function sendFunc () {
         loading.value = false;
     }
 }
+
+
+onMounted( () => {
+    let map = $leaflet.map('map').setView([35.700105, 51.400394], 14);
+        let tiles = $leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1
+        }).addTo(map);
+        let marker = $leaflet.marker([35.700105, 51.400394],  {
+        icon: $leaflet.icon({
+            popupAnchor: [12, 6],
+            iconUrl: 'images/map/marker-icon.png',
+            shadowUrl: 'images/map/marker-shadow.png'
+        })
+    }).addTo(map)
+            .bindPopup('<b>Hello There</b><br />I am a popup').openPopup();
+})
 
 </script>
